e300efecd2ef01b9ee67713de5299ff33390e5c1
Calum McConnell <calumlikesapplepie@gmail.com>
2020-06-22 00:20:22 -0400

    write some FunctionDeclarations examples

--- FunctionDeclarations.cpp
+++ FunctionDeclarations.cpp
@@ -1,1 +1,12 @@
-#include
+#include <iostream>
+
+int main(){
+    someFunction();
+}
+
+void someFunction(){
+    int x(0);        //one way to initialize a variable
+    int y = 0;   //another way to initialize a variable
+    int z{1};//yet ANOTHER way to initialize a variable.
+
+}
