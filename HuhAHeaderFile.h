ab1ed45474b76101201a363fae00b394ada4ad1e
Calum McConnell <calumlikesapplepie@gmail.com>
2020-06-23 13:09:43 -0400

    MoreRefrenceWork

--- HuhAHeaderFile.h
+++ HuhAHeaderFile.h
@@ -3,7 +3,10 @@
 //NOTE: It'd be a good idea to put a header guard here
 
 namespace tdi{
-    void printNice(std::string str){
+    void print(const std::string& str){
         std::cout << str << std::endl;
+    }
+    void print(const int& num){
+        print(std::to_string(num));
     }
 }
---
+++ MoreRefefrenceShennanagans.cpp
@@ -0,0 +1,15 @@
+#include "HuhAHeaderFile.h"
+
+int main(){
+    int numA = 4;
+    int numB = 10;
+    int& numRefA = numA;
+    int& numRefB = numB;
+    int& numRefC = numA;
+    int& numRefD = 4;
+    tdi::print(numRefA);
+    numRefA = 11;
+    tdi::print(numRefC++);// the ++ operator increments a number by 1.
+    //Putting it after the variable name means it gets incremented after the variable is used (post-increment)
+    tdi::print(numA);
+}
