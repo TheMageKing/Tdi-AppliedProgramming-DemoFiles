0f00120bf6536eb02bf6f41742e4254376f4ff45
Windzyboy <63932310+Windzyboy@users.noreply.github.com>
2020-06-23 13:26:07 -0400

    firstcommit
    yes

---
+++ TDI1.cpp
@@ -0,0 +1,63 @@
+                /*.,-:;//;:=,
+          . :H@@@MM@M#H/.,+%;,
+       ,/X+ +M@@M@MM%=,-%HMMM@X/,
+     -+@MM; $M@@MH+-,;XMMMM@MMMM@+-
+    ;@M@@M- XM@X;. -+XXXXXHHH@M@M#@/.
+  ,%MM@@MH ,@%=             .---=-=:=,.
+  =@#@@@MX.,                -%HX$$%%%:;
+ =-./@M@M$                   .;@MMMM@MM:
+ X@/ -$MM/                    . +MM@@@M$
+,@M@H: :@:                    . =X#@@@@-
+,@@@MMX, .                    /H- ;@M@M=
+.H@@@@M@+,                    %MM+..%#$.
+ /MMMM@MMH/.                  XM@MH; =;
+  /%+%$XHH@$=              , .H@@@@MX,
+   .=--------.           -%H.,@@@@@MX,
+   .%MM@@@HHHXX$$$%+- .:$MMX =M@@MM%.
+     =XMMM@MM@MM#H;,-+HMM@M+ /MMMX=
+       =%@M@M#@$-.=$@MM@@@M; %M%=
+         ,:+$+-,/H#MMMMMMM@= =,
+               =++%%%%+/:-.*/
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    //std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    
+    system("color a");
+    std::string initial = "I'll have 2 number 9s, ";
+    std::cout << initial;
+    _sleep(1000);
+    std::string a1 = "a number 9 large, ";
+    std::cout << a1;
+    _sleep(1000);
+    std::string a2 = "a number 6 with extra dip, ";
+    std::cout << a2;
+    _sleep(1000);
+    std::string a3 = "a number 7, ";
+    std::cout << a3;
+    _sleep(1000);
+    std::string a4 = "2 number 45s, ";
+    std::cout << a4;
+    _sleep(1000);
+    std::string a5 = "1 with cheese, ";
+    std::cout << a5;
+    _sleep(1000);
+    std::string a6 = "and a large soda.";
+    std::cout << a6;
+    _sleep(2000);
+
+    /*for (const std::string& word : msg)
+    {
+        std::cout << word << " ";
+    }*/
+
+    std::cout << std::endl;
+    std::string a7 = "Ok sir, have a nice day.";
+    std::cout << a7;
+    _sleep(2000);
+} 
