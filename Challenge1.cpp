42e6eff6a57d1d2e6c53db4619e8cef7a470d516
Calum McConnell <calumlikesapplepie@gmail.com>
2020-06-23 13:52:24 -0400

    updates

--- Challenge1.cpp
+++ Challenge1.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include "HuhAHeaderFile.h"
 
+double calculateDistance(int x1, int x2, int y1, int y2);
 int someFunction(){
     int x1;
     int x2;
     int y1;
     int y2;
+    double distance;
     tdi::print("x1: ");
     std::cin >> x1;
+    tdi::print("x2: ");
     std::cin >> x2;
+    tdi::print("y1: ");
     std::cin >> y1;
-    return x1;
+    tdi::print("y2: ");
+    std::cin >> y2;
+
+    return calculateDistance(x1, x2, y1, y2);
     }
 
+{
+
+}
+
+
 int main(){
-    std::cout << someFunction();
+    std::cout << someFunction(1, 2.3);
     tdi::print("x");
 }
